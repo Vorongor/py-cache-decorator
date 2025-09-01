b65d9cc10cdb18cb99a0f9b2cf09364f119d0398
Vorongor <mr.maddarknes@gmail.com>
2025-09-01 20:12:48 +0300

    Write the cache func and add two decorated fuck from exaple for check the code

--- app/main.py
+++ app/main.py
@@ -1,6 +1,37 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    store = {}
+
+    def wrapper(*args, **kwargs) -> Callable:
+        key = (args, frozenset(kwargs.items()))
+
+        if key in store:
+            print("Getting from cache")
+            return store[key]
+
+        result = func(*args, **kwargs)
+        store[key] = result
+        print("Calculating new result")
+        return result
+
+    return wrapper
+
+
+@cache
+def long_time_func(num1: int, num2: int, num3: int) -> int:
+    return (num1 ** num2 ** num3) % (num1 * num3)
+
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> list[Any]:
+    return [number ** power for number in n_tuple]
+
+
+long_time_func(1, 2, 3)
+long_time_func(2, 2, 3)
+long_time_func_2((5, 6, 7), 5)
+long_time_func(1, 2, 3)
+long_time_func_2((5, 6, 7), 10)
+long_time_func_2((5, 6, 7), 10)
