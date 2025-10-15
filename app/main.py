9409825215fdaaa21d69b8de8b3e51b85708145f
Vorongor <mr.maddarknes@gmail.com>
2025-10-15 13:28:06 +0300

    Remove unnecessary code

--- app/main.py
+++ app/main.py
@@ -27,11 +27,3 @@ def long_time_func(num1: int, num2: int, num3: int) -> int:
 @cache
 def long_time_func_2(n_tuple: tuple, power: int) -> list[Any]:
     return [number ** power for number in n_tuple]
-
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
